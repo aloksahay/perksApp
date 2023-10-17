b3b272630e5f3185cfd4eb057e902eef67d067f5
Steve Smith <stevysmith.ss@gmail.com>
2023-10-17 11:27:30 +0100

    Add Push & UniPass integration

---
+++ perks/perks/perksApp.swift
@@ -0,0 +1,17 @@
+//
+//  perksApp.swift
+//  perks
+//
+//  Created by Steve Smith on 16/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct perksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
