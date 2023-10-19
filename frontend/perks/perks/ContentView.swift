0a98ae1a2e612eb7025c50f5899498c1887c7636
Steve Smith <stevysmith.ss@gmail.com>
2023-10-19 16:44:36 +0100

    Push calls

--- frontend/perks/perks/ContentView.swift
+++ frontend/perks/perks/ContentView.swift
@@ -42,113 +42,122 @@ struct ContentView: View {
                            self.smartAccount = CustomAuthSdk.SmartAccount(options: options)
                            let initOptions = SmartAccountInitOptions(chainId: ChainID.POLYGON_MUMBAI)
                            try! await self.smartAccount!.initialize(options: initOptions)
-//                           self.tx = Shared.Transaction(to: "0x93f2e90Ab182E445E66a8523B57B3443cb0f1fC2", data: "0x", value: "0x1")
                             
-//                            let address = try await self.smartAccount!.address()
-//                            print("Current address")
-//                            print(address)
-//                            if let _user = try await PushUser.get(
-//                                account: address, env: .STAGING) {
-//                                self.user = _user
-//                                self.text = _user.profile.name ?? "User found: No Name"
-//                                print("got user")
-//                            } else {
-//                                let newUser = try await PushUser.createUserEmpty(userAddress: address, env: .STAGING)
-//                                self.text = newUser.profile.name ?? "User found: No Name"
-//                                self.user = newUser
-//                                print("created user")
-//                            }
-//                            
-//                            let channelAddress = "0xCc985ba6934d134Feec4824ba40258608F3A4333"
-//                            let res:PushChannel? = try await PushChannel.getChannel(
-//                                options: PushChannel.GetChannelOption(
-//                                    channel: channelAddress, env: .STAGING
-//                                ))
+                            let address = try await self.smartAccount!.address()
+                            print("Current address")
+                            print(address)
+                            if let _user = try await PushUser.get(
+                                account: address, env: .STAGING) {
+                                self.user = _user
+                                self.text = _user.profile.name ?? "User found: No Name"
+                                print("got user")
+                            } else {
+                                let newUser = try await PushUser.createUserEmpty(userAddress: address, env: .STAGING)
+                                self.text = newUser.profile.name ?? "User found: No Name"
+                                self.user = newUser
+                                print("created user")
+                            }
                             
-//                            print(res)
+                            let channelAddress = "0xCc985ba6934d134Feec4824ba40258608F3A4333"
+                            let res:PushChannel? = try await PushChannel.getChannel(
+                                options: PushChannel.GetChannelOption(
+                                    channel: channelAddress, env: .STAGING
+                                ))
                             
-//                            let signer = SmartAccountSigner(account: self.smartAccount!)
-//                            
-//                            
-//                            let user: PushUser?
-//                            if let _user: PushUser = try await PushUser.get(account: address, env: ENV.STAGING) {
-//                                user = _user
-//                            } else {
-//                                let _user:PushUser = try await PushUser.create(
-//                                    options: PushUser.CreateUserOptions(
-//                                        env: ENV.STAGING,
-//                                        signer: signer,
-//                                        progressHook: nil
-//                                    ))
-//                                user = _user
-//                            }
-//                            
-//                            print(user)
-//                            
-//                            let pgpPrivateKey = try await PushUser.DecryptPGPKey(
-//                              encryptedPrivateKey: user!.encryptedPrivateKey,
-//                              signer: signer
-//                            )
-//                            
-//                            let response:Message = try await PushChat.send(PushChat.SendOptions(
-//                              messageContent: "Gm gm! It's me... Mario",
-//                              messageType: "Text",
-//                              receiverAddress: "0xCc985ba6934d134Feec4824ba40258608F3A4333", 
-//                              account: address,
-//                              pgpPrivateKey: pgpPrivateKey,
-//                              env: ENV.STAGING
-//                            ))
-//                            
-//                            print("CHAT")
-//                            print(response)
-//                            
-//                            let typedSigner = TypedSmartAccountSigner(account: self.smartAccount!)
-//                            
-//                            let subscribers:PushChannel.ChannelSubscribers = try await PushChannel.getSubscribers(
-//                                option: PushChannel.GetChannelSubscribersOptions(
-//                                    channel: channelAddress,
-//                                    page: 1, limit: 10, env: .STAGING))
-//                            
-//                            print(subscribers)
-//
-//                            let isOptIn:Bool = try await PushChannel.getIsSubscribed(
-//                                  userAddress: address, channelAddress: channelAddress, env: .STAGING)
-//                            
-//                            print("Is subscribed?")
-//                            print(isOptIn)
-//                            
-//                            let createGroupOptions = try PushChat.CreateGroupOptions(
-//                              name: "Testing Perk",
-//                              description: "Perk Description",
-//                              image:
-//                                "data:image/png;base64,OTMNCia0/QpFcVIfFli8a+k6MsQ3cpgVE1O7sewD9UInj1Cxs6VSBVJgRZwOlt99dJe8Snw+UV9vzqLUYe9+uGXwYmSlxQgW7cBcNM5mZK5X8Bklicpy7LCeMM+d8sRhSXICZrPIX1N3LmKgX0npT70wH7uEPWoik9d7ZsHdmL+YccdAB4vZNILVoawSd7ZpuG7RbyX1lneV7YQvnIqklfuum72WaGUL9UFPVQC1v6KN5v1HD3wef46AUIWPUZtvmuzrnsH0A65sWhfpfILQsdyOFchUUd0rIc1+nx5WjSfn11OsPIFioNYTAykrUNdW+MOvQF6O5cw0C9C4CeolEFR/ah/QVFOF3oGstDtkTnmeOQYLyFMFCteXRfjKHdTyOdoPsbWmN2xyWxnmdepnlQKxj7NQTW1knLT4xr2TewU68Jn/wm8pWKPLK/eNvPuoFF27wLmw2AlHKbRl6AzDJ2oa3zpDb2yaytzAJ9bS0Z7dKNPAeqjD8I96ASEN8blrF4xTNw3X3ZhfPxhOcZpZpuSY+XM8g71+ZgUpvzd59Lp3419he8sT+txDzxGBM5orR62mJCIi4kIoFjA2vWZqOL4Ire7dxdEoXR6x2ce0hD5d86MTSPn+BkpDgsqxcHixvbaGh4Hh/Y26eD8Y7NfMosQ9JSxfhgQnMxuNjwAkpUo=",
-//                              members: ["0xcc985ba6934d134feec4824ba40258608f3a4333"], // Max 10
-//                              isPublic: false, // Only in private group messages are encrypted
-//                              creatorAddress: address,
-//                              creatorPgpPrivateKey: pgpPrivateKey,
-//                              env: ENV.STAGING
-//                            )
-//
-//                            let group:PushChat.PushGroup = try await PushChat.createGroup(options: createGroupOptions)
-//                            
-//                            print("Group chat")
-//                            print(group)
-//                            
-//                            let result:Bool = try await PushChannel.unsubscribe(
-//                                option: PushChannel.SubscribeOption(
-//                                    signer: typedSigner,
-//                                    channelAddress: channelAddress,
-//                                    env: .STAGING))
-//                            
-//                            print("unsubscribe")
-//                            print(result)
-//
-//                            let subscribeResult:Bool = try await PushChannel.subscribe(
-//                                option: PushChannel.SubscribeOption(
-//                                    signer: typedSigner,
-//                                    channelAddress: channelAddress,
-//                                    env: .STAGING))
                             
+                            
+                            print(res)
+                            
+                            let signer = SmartAccountSigner(account: self.smartAccount!)
+                            
+                            
+                            let user: PushUser?
+                            if let _user: PushUser = try await PushUser.get(account: address, env: ENV.STAGING) {
+                                user = _user
+                            } else {
+                                let _user:PushUser = try await PushUser.create(
+                                    options: PushUser.CreateUserOptions(
+                                        env: ENV.STAGING,
+                                        signer: signer,
+                                        progressHook: nil
+                                    ))
+                                user = _user
+                            }
+                            
+                            print(user)
+                                                        
+                            let pgpPrivateKey = try await PushUser.DecryptPGPKey(
+                              encryptedPrivateKey: user!.encryptedPrivateKey,
+                              signer: signer
+                            )
+                            
+                            let response:Message = try await PushChat.send(PushChat.SendOptions(
+                              messageContent: "Gm gm! It's me... Mario",
+                              messageType: "Text",
+                              receiverAddress: "0xCc985ba6934d134Feec4824ba40258608F3A4333", 
+                              account: address,
+                              pgpPrivateKey: pgpPrivateKey,
+                              env: ENV.STAGING
+                            ))
+                            
+                            print("CHAT")
+                            print(response)
+                            
+                            let typedSigner = TypedSmartAccountSigner(account: self.smartAccount!)
+                            
+                            let subscribers:PushChannel.ChannelSubscribers = try await PushChannel.getSubscribers(
+                                option: PushChannel.GetChannelSubscribersOptions(
+                                    channel: channelAddress,
+                                    page: 1, limit: 10, env: .STAGING))
+                            
+                            print(subscribers)
+                        
+                            let createGroupOptions = try PushChat.CreateGroupOptions(
+                              name: "Testing Perk",
+                              description: "Perk Description",
+                              image:
+                                "data:image/png;base64,OTMNCia0/QpFcVIfFli8a+k6MsQ3cpgVE1O7sewD9UInj1Cxs6VSBVJgRZwOlt99dJe8Snw+UV9vzqLUYe9+uGXwYmSlxQgW7cBcNM5mZK5X8Bklicpy7LCeMM+d8sRhSXICZrPIX1N3LmKgX0npT70wH7uEPWoik9d7ZsHdmL+YccdAB4vZNILVoawSd7ZpuG7RbyX1lneV7YQvnIqklfuum72WaGUL9UFPVQC1v6KN5v1HD3wef46AUIWPUZtvmuzrnsH0A65sWhfpfILQsdyOFchUUd0rIc1+nx5WjSfn11OsPIFioNYTAykrUNdW+MOvQF6O5cw0C9C4CeolEFR/ah/QVFOF3oGstDtkTnmeOQYLyFMFCteXRfjKHdTyOdoPsbWmN2xyWxnmdepnlQKxj7NQTW1knLT4xr2TewU68Jn/wm8pWKPLK/eNvPuoFF27wLmw2AlHKbRl6AzDJ2oa3zpDb2yaytzAJ9bS0Z7dKNPAeqjD8I96ASEN8blrF4xTNw3X3ZhfPxhOcZpZpuSY+XM8g71+ZgUpvzd59Lp3419he8sT+txDzxGBM5orR62mJCIi4kIoFjA2vWZqOL4Ire7dxdEoXR6x2ce0hD5d86MTSPn+BkpDgsqxcHixvbaGh4Hh/Y26eD8Y7NfMosQ9JSxfhgQnMxuNjwAkpUo=",
+                              members: ["0xcc985ba6934d134feec4824ba40258608f3a4333"], // Max 10
+                              isPublic: false, // Only in private group messages are encrypted
+                              creatorAddress: address,
+                              creatorPgpPrivateKey: pgpPrivateKey,
+                              env: ENV.STAGING
+                            )
+
+                            let group:PushChat.PushGroup = try await PushChat.createGroup(options: createGroupOptions)
+                            
+                            print("Group chat")
+                            print(group)
+                            
+                            let testChannelAddress = "0xa2841271Fc88D8db0338BeF15eBeB1FE6c3E960F"
+//                            staging.push.org/channels?channel=0xCc985ba6934d134Feec4824ba40258608F3A4333
+
+                            let mockSigner = MockEIP712OptinSigner()
+                            let userAddress = try await mockSigner.getAddress()
+
+                            let subscribe = try await PushChannel.subscribe(
+                              option: PushChannel.SubscribeOption(
+                                signer: mockSigner, channelAddress: testChannelAddress, env: .STAGING))
+
+                            let isOptIn = try await PushChannel.getIsSubscribed(
+                              userAddress: userAddress, channelAddress: testChannelAddress, env: .STAGING)
+                            
+                            print("Is subscribed?")
+                            print(isOptIn)
+                            
+                            let outMockSigner = MockEIP712OptoutSigner()
+                            let outUserAddress = try await outMockSigner.getAddress()
+
+                            let unsub = try await PushChannel.unsubscribe(
+                              option: PushChannel.SubscribeOption(
+                                signer: outMockSigner, channelAddress: testChannelAddress, env: .STAGING))
+
+                            let isOptOut = try await PushChannel.getIsSubscribed(
+                              userAddress: outUserAddress, channelAddress: testChannelAddress, env: .STAGING)
+
+                            print("Is subscribed?")
+                            print(isOptOut)
+
                             isLogged = true
                         }
                     }
@@ -195,6 +204,39 @@ class TypedSmartAccountSigner: Push.TypedSigner {
         return try await account.address()
     }
 }
+
+public struct MockEIP712OptinSigner: TypedSigner {
+  public func getEip712Signature(message: String)
+    async throws -> String
+  {
+    // optin
+    //channel    eip155:5:0x2AEcb6DeE3652dA1dD6b54D5fd4f7D8F43DaEb78
+    //subscriber eip155:5:0xD26A7BF7fa0f8F1f3f73B056c9A67565A6aFE63c
+    return
+      "0xbd2724da36cbb3a99d59d4133b9cceb6a602bb1c0aab69d249a199c071a196880e8b7fba882cb10e943223be2ce34ccc5ceb4e1326410e968cd4497748c0de111c"
+  }
+
+  public func getAddress() async throws -> String {
+    return "0xD26A7BF7fa0f8F1f3f73B056c9A67565A6aFE63c"
+  }
+}
+
+public struct MockEIP712OptoutSigner: TypedSigner {
+  public func getEip712Signature(message: String)
+    async throws -> String
+  {
+    // optout
+    //channel    eip155:5:0x2AEcb6DeE3652dA1dD6b54D5fd4f7D8F43DaEb78
+    //subscriber eip155:5:0xD26A7BF7fa0f8F1f3f73B056c9A67565A6aFE63c
+    return
+      "0xac77e24153f6b5a46b42020ba987d402c4b5b0308aa62cf06cd2a9173c3c613d4d182bba1607d004a504848cebe30b73c9efb88897e0f9563f38e21ec2e84b281b"
+  }
+
+  public func getAddress() async throws -> String {
+    return "0xD26A7BF7fa0f8F1f3f73B056c9A67565A6aFE63c"
+  }
+}
+
 
 
 class SmartAccountSigner: Push.Signer {
